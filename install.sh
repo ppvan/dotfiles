8755db798af1d244a88b97957237f4058fb204d0
ppvan <phuclaplace@gmail.com>
2024-03-07 17:19:54 +0700

    feat: add gnome setup

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 dotfiledir="${HOME}/dotfiles"
 
 # list of files/folders to symlink in ${homedir}
-files=(bashrc bash_profile bash_prompt bash_alias )
+files=(bashrc bash_profile bash_prompt bash_alias gitconfig)
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
@@ -22,16 +22,37 @@ for file in "${files[@]}"; do
     ln -sf "${dotfiledir}/.${file}" "${HOME}/.${file}"
 done
 
-# Run the MacOS Script
-./macOS.sh
+# Copy some custom desktop files
+echo "Install Custom Desktop Files"
+for file in "${dotfiledir}"/.local/share/applications/*; do
+    echo "$(basename ${file}) -> ${HOME}/.local/share/applications/$(basename ${file})"
+    sleep 0.1
+    ln -sf "${file}" "${HOME}/.local/share/applications/$(basename ${file})"
+done
 
-# Run the Homebrew Script
-./brew.sh
 
-# Run VS Code Script
-./vscode.sh
+echo "Install Custom Bash Completions"
+for file in "${dotfiledir}"/.local/share/bash-completion/completions/*; do
+    
+    echo "$(basename ${file}) -> ${HOME}/.local/share/bash-completion/completions/$(basename ${file})"
+    sleep 0.1
+    ln -sf "${file}" "${HOME}/.local/share/bash-completion/completions/$(basename ${file})"
+    sleep 0.1
+done
 
-# Run the Sublime Script
-./sublime.sh
+# Fonts
+for font in "${dotfiledir}"/.local/share/fonts/*; do
+    echo "Installing $(basename ${font}) font"
+    sleep 0.1
+    cp -r "${file}" "${HOME}/.local/share/fonts/"
+done
+
+
+# Firefox config
+echo "Install Firefox Config"
+cp -R "${dotfiledir}"/.config/firefox/* "${HOME}"/.config/firefox/
+
+
+
 
 echo "Installation Complete!"
